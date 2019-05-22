b5617cdcd8eb179eb314990ad834cad16c5cd753
ValerioNunes <valeriobsno@gmail.com>
2019-05-21 23:53:53 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:oficina_app/pages/home_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new HomePage( userId: "01547492"));
+  }
+}
